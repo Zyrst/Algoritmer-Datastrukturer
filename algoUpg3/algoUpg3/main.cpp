7f6d99943cba63b0285f25b73c338c9b8c241700
pallevante <pallevante@live.se>
2014-12-07 12:06:19 +0100

    Hårdkodat fram trädet så vi vet ungefär hur det kommer se ut.

--- algoUpg3/algoUpg3/main.cpp
+++ algoUpg3/algoUpg3/main.cpp
@@ -4,9 +4,19 @@
 int main()
 {
 	std::priority_queue<TreeWrapper> q;
-	q.push(TreeWrapper(new Tree(4, 'a')));
-	q.push(TreeWrapper(new Tree(3, 'b')));
-	q.push(TreeWrapper(new Tree(1, 'c')));
+
+	Tree* a		= new Tree(4, 'a');
+	Tree* b		= new Tree(3, 'b');
+	Tree* c		= new Tree(1, 'c');
+	Tree* node	= new Tree(0, ' ');
+	
+	Tree* node0 = new Tree((c->getWeight() + b->getWeight()), c, b);
+	Tree* root	= new Tree((node0->getWeight() + a->getWeight()), node0, a);
+
+	q.push(TreeWrapper(root));
+	q.push(TreeWrapper(a));
+	q.push(TreeWrapper(b));
+	q.push(TreeWrapper(c));
 
 	return 0;
 }
