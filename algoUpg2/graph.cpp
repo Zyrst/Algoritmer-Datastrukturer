ad71bf3185227113100185641adc273cbf31642d
Pallevante <pallevante@live.se>
2014-12-09 11:36:59 +0100

    Tagit bort lite mer onödiga grejer. Kollar bara djupet direkt och lägger till som fiende eller vän

--- algoUpg2/graph.cpp
+++ algoUpg2/graph.cpp
@@ -30,69 +30,17 @@ void Graph::drawGraph(Person* person)
  				//Adds the first enemy found.
 	 			if(!mEnemy->marked)
  				{
- 					mDefault->addFriend(mEnemy);
- 					std::pair<int, bool> _set(currentPaths, true);
- 					std::pair<Person*, std::pair<int, bool>> EnemyPair(mEnemy, _set);
- 					vec.push_back(EnemyPair);
- 					mEnemy->marked = true;
+ 					if(currentPaths % 2 == 0)
+ 						mDefault->addFriend(mEnemy);
+ 					else
+ 						mDefault->addToUnfriend(mEnemy);
  					return;
  				}
- 				cout << mEnemy->mName << " is enemy of: " << mPerson->mName<<endl;
  				tempQ.pop();	
  			}
 		}
  		moveToNextElement();
 	}
-	draw(vec);
-}
-
-// recives a package in form of a vector containing pairs with person and a pair with relevant data.
-void Graph::draw(std::vector<pair<Person*, pair<int, bool>>> _s) 
-{
-	for(int i =	0; i < _s.size(); i++)
-	{
-		Person* 				p = _s[i].first;
-		std::pair<int, bool> 	v = _s[i].second;
-
-		if(v.first % 2 == 0)
-			mDefault->addFriend(p);
-		else 
-			mDefault->addToUnfriend(p);
-	}
-}
-
-
-
-
-//Returns false if it's an enemy
-//else true
-bool Graph::isNotEnemy(Person* person)
-{
-	std::queue<Person*> tempQ = mDefault->enemies;
-	for (int i = 0; i < tempQ.size(); i++)
-	{
-		if (tempQ.front() == person)
-		{
-			return false;
-		}
-		else
-			tempQ.pop();
-	}
-	return true;	
-}
-// Returns false if person is found in friends.
-bool Graph::isNotFriend(Person* person)
-{
-	std::queue<Person*> tempQ = mDefault->friends;
-	for (int i = 0; i < tempQ.size(); i++)
-	{
-		if(tempQ.front() == person)
-		{
-			return false;
-		}
-		tempQ.pop();
-	}
-	return true;
 }
 
 void Graph::moveToNextElement()
